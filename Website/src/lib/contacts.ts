13ae2e191ccd6b6c64c2c49853548d4d8c19aff9
BrunoAFonseca <brunoaf@outlook.com>
2024-09-26 15:11:24 -0300

    Removendo todos os arquivos do repositório

--- Website/src/lib/contacts.ts
+++
@@ -1,9 +0,0 @@
-const ciberdemLink = "https://ciberdem.mack.com.br/";
-const ciberdemGitHub = "https://github.com/ciberdem";
-
-const contacts = [
-  { label: "CIBERDEM", path: ciberdemLink },
-  { label: "GitHub", path: ciberdemGitHub },
-];
-
-export default contacts;
--- Website/src/lib/routes.ts
+++
@@ -1,8 +0,0 @@
-const routes = [
-  { label: "Notícias", path: "/noticias" },
-  { label: "Resultados", path: "/resultados" },
-  { label: "Membros", path: "/membros" },
-  { label: "Sobre", path: "/sobre" },
-];
-
-export default routes;
